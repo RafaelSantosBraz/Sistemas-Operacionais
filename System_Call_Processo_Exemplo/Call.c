9b07d5d4edb460b1ae71f80f0634857b4e0d4487
RafaelSantosBraz <rafinha.santos.braz@gmail.com>
2018-04-16 11:12:31 -0300

    Correção System call

--- System_Call_Processo_Exemplo/Call.c
+++ System_Call_Processo_Exemplo/Call.c
@@ -5,19 +5,27 @@
 
 int main (int argc, char *argv[], char *envp[])
 {
+    // identificação do processo
     int pid;
+    // criação de um processo filho
     pid = fork();
+    // erro de criação
     if (pid < 0)
     {
         perror ("Error: ");
         exit(-1);
     }
-    else if (pid > 0)
+    // ainda é o processo pai
+     else if (pid > 0)
     {
+        // bloqueia o pai e executa o filho
         wait(0);
     }
+    // execução pelo processo filho (= 0)
     else
     {
+        getchar();
+        // troca de programa do processo filho
         execve("/bin/date", argv, envp);
         perror("Error: ");
     }
