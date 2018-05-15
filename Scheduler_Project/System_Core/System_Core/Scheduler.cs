484bfdce281aa954f56257c499fc029bb22a2c6c
Rafael dos Santos Braz <rafinha.santos.braz@gmail.com>
2018-05-15 19:40:56 -0300

    Início da implementação FIFO

--- Scheduler_Project/System_Core/System_Core/Scheduler.cs
+++ Scheduler_Project/System_Core/System_Core/Scheduler.cs
@@ -10,6 +10,45 @@ namespace System_Core
     {
         private static List<Container> ready = new List<Container>();
         private static int rule = 1;
-        
+        static Object a;
+
+        public static Container select()
+        {
+            switch (rule)
+            {
+                case 1:
+                    return FIFO();
+                default:
+                    return null;
+            }
+        }
+
+        private static bool change_rule(int new_rule)
+        {
+            if (new_rule > 0 && new_rule < 6)
+            {
+                return false;
+            }
+            else
+            {
+                rule = new_rule;
+                return true;
+            }
+        }
+
+        private static Container FIFO()
+        {
+            if (ready.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                Container selected = ready.ElementAt(0);
+                ready.RemoveAt(0);
+                ready.Add(selected);
+                return selected;
+            }
+        }
     }
 }
