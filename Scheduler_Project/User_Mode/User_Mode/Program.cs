83423994717e5dd00f87566155ee2cf2ef41c0ff
Rafael dos Santos Braz <rafinha.santos.braz@gmail.com>
2018-05-16 10:30:35 -0300

    Criação de Modo Usuário

---
+++ Scheduler_Project/User_Mode/User_Mode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System_Core;
+
+namespace User_Mode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Process> processes = new List<Process>();
+            for (int i = 0; i < 10; i++)
+            {
+                processes.Add(new Process(i));
+            }
+            Kernel.load_processes(processes);
+            Console.ReadKey();
+        }
+    }
+}
